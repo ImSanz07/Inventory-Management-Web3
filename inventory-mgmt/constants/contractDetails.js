50d7325e5cc79a588124d18636f22579ef1c6afc
Sanat Patil <kingsanz007@gmail.com>
2024-11-12 02:00:26 +0530

    first commit

---
+++ inventory-mgmt/constants/contractDetails.js
@@ -0,0 +1,5 @@
+import contractAbi from "../src/app/Inventory.json";
+
+// Replace this with your actual deployed contract address
+export const CONTRACT_ADDRESS = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9';
+export const CONTRACT_ABI = contractAbi.abi;
